590500b502a0d020bfbcf8f86d230f2ba9771735
김범주 <kbjid17@naver.com>
2022-03-16 13:56:41 +0900

    .gitIgnore update 및 명세서 중간 저장

--- skeleton-project_nft/smart-contracts/contracts/SsafyNFT.sol
+++ skeleton-project_nft/smart-contracts/contracts/SsafyNFT.sol
@@ -2,29 +2,54 @@
 pragma solidity ^0.8.4;
 
 import "./token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
 /**
  * PJT Ⅰ - 과제 2) NFT Creator 구현
  * 상태 변수나 함수의 시그니처는 구현에 따라 변경할 수 있습니다.
  */
-contract SsafyNFT {
-
-    uint256 private _tokenIds;
+contract SsafyNFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
     mapping(uint256 => string) tokenURIs;
 
-    constructor() {
+    constructor() ERC721("SsafyNFT", "SFT") {
+        //erc721 생성사 호출
         // TODO
     }
 
-    function current() public view returns (uint256) {
-        return _tokenIds;
+    // function current() public view returns (uint256) {
+    //     return _tokenIds;
+    // }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721)
+        returns (string memory)
+    {
+        // TODO
+        return tokenURIs[tokenId];
     }
 
-    function tokenURI(uint256 tokenId) public view returns (string memory) {
+    /*
+    호출자가 지정한 tokenURI를 새롭게 발행
+    내부적으로 새로운 토큰 식별자를 부여받고 _mint()를 호출
+    상태 변수에 토큰 식별자의 tokenURI 정보를 추가
+    새롭게 생성된 토큰 식별자를 반환
+    */
+    function create(address to, string memory _tokenURI)
+        public
+        returns (uint256)
+    {
         // TODO
-    }
+        _tokenIds.increment();
 
-    function create(address to, string memory _tokenURI) public returns (uint256) {
-        // TODO
+        uint256 newItemid = _tokenIds.current();
+        _mint(to, newItemid);
+        tokenURIs[newItemid] = _tokenURI;
+
+        return newItemid;
     }
 }
--- skeleton-project_nft/smart-contracts/contracts/token/ERC721/IERC721.sol
+++ skeleton-project_nft/smart-contracts/contracts/token/ERC721/IERC721.sol
@@ -1,143 +1,403 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts v4.3.2 (token/ERC721/IERC721.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
-import "../../utils/introspection/IERC165.sol";
+/*
+'ERC165.sol'에서는 인터페이스 ID를 가져와서 사용가능한 인터페이스인지 확인했다.
+'IERC721.sol'은 ERC721과 인터페이스 역할을 하며 IERC721의 함수를 ERC721에서 상속하여 사용했다.
+*/
+import "./IERC721.sol";
+import "./IERC721Receiver.sol";
+import "./extensions/IERC721Metadata.sol";
+import "../../utils/Address.sol";
+import "../../utils/Context.sol";
+import "../../utils/Strings.sol";
+import "../../utils/introspection/ERC165.sol";
 
 /**
- * @dev Required interface of an ERC721 compliant contract.
+ * PJT Ⅰ - 과제 1 ERC-721 구현
+ * @dev EIP-721을 준수하여 ERC721을 작성합니다.
+ * https://eips.ethereum.org/EIPS/eip-721[ERC721] Non-Fungible Token Standard
  */
-interface IERC721 is IERC165 {
-    /**
-     * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
-     */
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
+    using Address for address;
+    using Strings for uint256;
+
+    // Token name
+    string private _name;
+
+    // Token symbol
+    string private _symbol;
+
+    // Mapping from token ID to owner address
+    mapping(uint256 => address) private _owners;
+
+    // Mapping owner address to token count
+    mapping(address => uint256) private _balances;
+
+    // Mapping from token ID to approved address
+    mapping(uint256 => address) private _tokenApprovals;
+
+    // Mapping from owner to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
 
     /**
-     * @dev Emitted when `owner` enables `approved` to manage the `tokenId` token.
+     * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection.
      */
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
 
     /**
-     * @dev Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
+     * @dev See {IERC165-supportsInterface}.
      */
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC165, IERC165)
+        returns (bool)
+    {
+        // TODO
+    }
 
     /**
-     * @dev Returns the number of tokens in ``owner``'s account.
+     * @dev See {IERC721-balanceOf}.
+     특정 Owner가 보유하고 있는 NFT 수 반환
      */
-    function balanceOf(address owner) external view returns (uint256 balance);
+    function balanceOf(address owner)
+        public
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        // TODO
+        require(
+            owner != address(0),
+            "ERC721: balance query for the zero address"
+        );
+        return _balances[owner];
+    }
 
     /**
-     * @dev Returns the owner of the `tokenId` token.
-     *
-     * Requirements:
-     *
-     * - `tokenId` must exist.
+     * @dev See {IERC721-ownerOf}.
+     특정 NFT의 소유자 주소를 반환
      */
-    function ownerOf(uint256 tokenId) external view returns (address owner);
+    function ownerOf(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (address)
+    {
+        // TODO
+        address owner = _owners[tokenId];
+        require(
+            owner != address(0),
+            "ERC721: owner query for nonexistent token"
+        );
+        return owner;
+    }
 
     /**
-     * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
-     * are aware of the ERC721 protocol to prevent tokens from being forever locked.
-     *
-     * Requirements:
-     *
-     * - `from` cannot be the zero address.
-     * - `to` cannot be the zero address.
-     * - `tokenId` token must exist and be owned by `from`.
-     * - If the caller is not `from`, it must be have been allowed to move this token by either {approve} or {setApprovalForAll}.
-     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
-     *
-     * Emits a {Transfer} event.
+     * @dev See {IERC721Metadata-name}.
+
      */
-    function safeTransferFrom(
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-symbol}.
+     */
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
+
+    function _exists(uint256 tokenId) internal view virtual returns (bool) {
+        return _owners[tokenId] != address(0);
+    }
+
+    function _baseURI() internal view virtual returns (string memory) {
+        return "";
+    }
+
+    function _approve(address to, uint256 tokenId) internal virtual {
+        _tokenApprovals[tokenId] = to;
+        emit Approval(ERC721.ownerOf(tokenId), to, tokenId);
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     특정 NFT의 원본 데이터가 어디에 저장되어 있는지 URL 반환
+     */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        // TODO
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory baseURI = _baseURI();
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, tokenId.toString()))
+                : "";
+    }
+
+    /**
+     * @dev See {IERC721-approve}.
+     특정 주소에게 호출 주소가 보유한 NFT의 전송 권한을 부여
+     require(조건, "문자열") 해당 조건을 만족하지 않을 시 문자열을 출력하며 실행 취소됨
+     */
+    function approve(address to, uint256 tokenId) public virtual override {
+        // TODO
+        address owner = ERC721.ownerOf(tokenId);
+        require(to != owner, "ERC721: approval to current owner");
+
+        require(
+            _msgSender() == owner || isApprovedForAll(owner, _msgSender()),
+            "ERC721: approve caller is not owner nor approved for all"
+        );
+
+        _approve(to, tokenId);
+    }
+
+    /**
+     * @dev See {IERC721-getApproved}.
+     특정 NFT의 전송 권한을 부여받은(approved) 주소를 반환
+     */
+    function getApproved(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (address)
+    {
+        // TODO
+        require(
+            _exists(tokenId),
+            "ERC721: approved query for nonexistent token"
+        );
+
+        return _tokenApprovals[tokenId];
+    }
+
+    function _setApprovalForAll(
+        address owner,
+        address operator,
+        bool approved
+    ) internal virtual {
+        require(owner != operator, "ERC721: approve to caller");
+        _operatorApprovals[owner][operator] = approved;
+        emit ApprovalForAll(owner, operator, approved);
+    }
+
+    /**
+     * @dev See {IERC721-setApprovalForAll}.
+     NFT 소유자가 해당 주소에게 모든 NFT의 전송 권한을 부여
+     */
+    function setApprovalForAll(address operator, bool approved)
+        public
+        virtual
+        override
+    {
+        // TODO
+        _setApprovalForAll(_msgSender(), operator, approved);
+    }
+
+    /**
+     * @dev See {IERC721-isApprovedForAll}.
+     특정 주소에게 모든 전송 권한이 있는지 판별
+     */
+    function isApprovedForAll(address owner, address operator)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        // TODO
+        return _operatorApprovals[owner][operator];
+    }
+
+    function _isApprovedOrOwner(address spender, uint256 tokenId)
+        internal
+        view
+        virtual
+        returns (bool)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721: operator query for nonexistent token"
+        );
+        address owner = ERC721.ownerOf(tokenId);
+        return (spender == owner ||
+            isApprovedForAll(owner, spender) ||
+            getApproved(tokenId) == spender);
+    }
+
+    function _beforeTokenTransfer(
         address from,
         address to,
         uint256 tokenId
-    ) external;
+    ) internal virtual {}
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual {}
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual {
+        require(
+            ERC721.ownerOf(tokenId) == from,
+            "ERC721: transfer from incorrect owner"
+        );
+        require(to != address(0), "ERC721: transfer to the zero address");
+
+        _beforeTokenTransfer(from, to, tokenId);
+
+        // Clear approvals from the previous owner
+        _approve(address(0), tokenId);
+
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+
+        _afterTokenTransfer(from, to, tokenId);
+    }
 
     /**
-     * @dev Transfers `tokenId` token from `from` to `to`.
-     *
-     * WARNING: Usage of this method is discouraged, use {safeTransferFrom} whenever possible.
-     *
-     * Requirements:
-     *
-     * - `from` cannot be the zero address.
-     * - `to` cannot be the zero address.
-     * - `tokenId` token must be owned by `from`.
-     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
-     *
-     * Emits a {Transfer} event.
+     * @dev See {IERC721-transferFrom}.
+     NFT 전송
      */
     function transferFrom(
         address from,
         address to,
         uint256 tokenId
-    ) external;
+    ) public virtual override {
+        // TODO
+        //solhint-disable-next-line max-line-length
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+
+        _transfer(from, to, tokenId);
+    }
 
     /**
-     * @dev Gives permission to `to` to transfer `tokenId` token to another account.
-     * The approval is cleared when the token is transferred.
-     *
-     * Only a single account can be approved at a time, so approving the zero address clears previous approvals.
-     *
-     * Requirements:
-     *
-     * - The caller must own the token or be an approved operator.
-     * - `tokenId` must exist.
-     *
-     * Emits an {Approval} event.
+     * @dev See {IERC721-safeTransferFrom}.
+     NFT 전송 시 받을 주소가 CA(Contract Address => 스마트 컨트렉트의 주소)가 아닌 EOA(Externally Owned Address)인지 확인하고 EOA만 전송
+     EOA,CA : 이더리움 계정을 의미함
+     EOA : 외부 소유 계정
+     - 주소(address) 와 개인키(public address) 로 구성되어 있음
+     - 이더를 송수신할 수 있으며 트랜잭션을 보낼 수 있음
+     CA : 컨트렉트 계정
+     - 개인키 x
+     - 스마트 컨트렉트를 블록체인에 배포할 때 생성됨
+     - 스마트 컨트렉트 코드를 담고 있으며, EOA와 마찬가지로 이더를 송신할 수 있으며 다른 컨트렉트나 EOA의 호출을 통해 트랜잭션을 발생시킬 수 있음
      */
-    function approve(address to, uint256 tokenId) external;
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override {
+        // TODO
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    function _safeTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) internal virtual {}
 
     /**
-     * @dev Returns the account approved for `tokenId` token.
-     *
-     * Requirements:
-     *
-     * - `tokenId` must exist.
-     */
-    function getApproved(uint256 tokenId) external view returns (address operator);
-
-    /**
-     * @dev Approve or remove `operator` as an operator for the caller.
-     * Operators can call {transferFrom} or {safeTransferFrom} for any token owned by the caller.
-     *
-     * Requirements:
-     *
-     * - The `operator` cannot be the caller.
-     *
-     * Emits an {ApprovalForAll} event.
-     */
-    function setApprovalForAll(address operator, bool _approved) external;
-
-    /**
-     * @dev Returns if the `operator` is allowed to manage all of the assets of `owner`.
-     *
-     * See {setApprovalForAll}
-     */
-    function isApprovedForAll(address owner, address operator) external view returns (bool);
-
-    /**
-     * @dev Safely transfers `tokenId` token from `from` to `to`.
-     *
-     * Requirements:
-     *
-     * - `from` cannot be the zero address.
-     * - `to` cannot be the zero address.
-     * - `tokenId` token must exist and be owned by `from`.
-     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
-     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
-     *
-     * Emits a {Transfer} event.
+     * @dev See {IERC721-safeTransferFrom}.
      */
     function safeTransferFrom(
         address from,
         address to,
         uint256 tokenId,
-        bytes calldata data
-    ) external;
+        bytes memory _data
+    ) public virtual override {
+        // TODO
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        _safeTransfer(from, to, tokenId, _data);
+    }
+
+    /**
+     * @dev Mints `tokenId` and transfers it to `to`.
+     *
+     * WARNING: Usage of this method is discouraged, use {_safeMint} whenever possible
+     *
+     * Requirements:
+     *
+     * - `tokenId` must not exist.
+     * - `to` cannot be the zero address.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _mint(address to, uint256 tokenId) internal virtual {
+        // TODO
+        require(to != address(0), "ERC721: mint to the zero address");
+        require(!_exists(tokenId), "ERC721: token already minted");
+
+        _beforeTokenTransfer(address(0), to, tokenId);
+
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(address(0), to, tokenId);
+
+        _afterTokenTransfer(address(0), to, tokenId);
+    }
+
+    /**
+     * @dev Destroys `tokenId`.
+     * The approval is cleared when the token is burned.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     *
+     * Emits a {Transfer} event.
+     특정 토큰을 없애는 함수
+     */
+    function _burn(uint256 tokenId) internal virtual {
+        // TODO
+        address owner = ERC721.ownerOf(tokenId);
+
+        _beforeTokenTransfer(owner, address(0), tokenId);
+
+        // Clear approvals
+        _approve(address(0), tokenId);
+
+        _balances[owner] -= 1;
+        delete _owners[tokenId];
+
+        emit Transfer(owner, address(0), tokenId);
+
+        _afterTokenTransfer(owner, address(0), tokenId);
+    }
 }
