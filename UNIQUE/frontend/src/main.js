1b46de450aba4c804ce189c3ba9d03d0e746090c
tg8685 <tg8685@gmail.com>
2022-03-17 23:34:05 +0900

    S06P22E205-52 feat:[FE] 메인 페이지 - 레이아웃 설정 및 프레임 제작

--- UNIQUE/frontend/src/main.js
+++ UNIQUE/frontend/src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 // import store from './store'
 
 // createApp(App).use(store).use(router).mount('#app')
@@ -9,32 +9,56 @@ import router from './router'
 const app = createApp(App);
 
 // bootstrap
-import "bootstrap"
+import "bootstrap";
 
 // vue select
-import vSelect from 'vue-select'
+import vSelect from "vue-select";
 import "vue-select/dist/vue-select.css";
 
-app.component('v-select', vSelect)
+app.component("v-select", vSelect);
 
 // clipboard
-import VueClipboard from 'vue3-clipboard'
+import VueClipboard from "vue3-clipboard";
 app.use(VueClipboard, {
-    autoSetContainer: true,
-    appendToBody: true,
-})
+  autoSetContainer: true,
+  appendToBody: true,
+});
 
 // template custom css
-import './assets/scss/bundles.scss'
-import './assets/scss/style.scss'
+import "./assets/scss/bundles.scss";
+import "./assets/scss/style.scss";
 
-import HeaderMain from './components/common/HeaderMain.vue';
-import LogoLink from './components/common/LogoLink.vue';
-import ButtonLink from './components/common/ButtonLink.vue';
-import ButtonGroup from './components/common/ButtonGroup.vue';
+import HeaderMain from "./components/common/HeaderMain.vue";
+import LogoLink from "./components/common/LogoLink.vue";
+import ButtonLink from "./components/common/ButtonLink.vue";
+import ButtonGroup from "./components/common/ButtonGroup.vue";
+import FullScreenSlider from "./components/common/FullScreenSlider.vue";
+import Featured from "./components/section/Featured.vue";
+import SectionHeading from "./components/common/SectionHeading.vue";
+import MainSectionHeading from "./components/common/MainSectionHeading.vue";
 
-app.component('HeaderMain', HeaderMain);
-app.component('LogoLink', LogoLink)
-app.component('ButtonLink', ButtonLink)
-app.component('ButtonGroup', ButtonGroup)
-app.use(router).mount('#app')
+import ProductsContainer from "./components/section/ProductsContainer.vue";
+import RelatedProduct from "./components/section/RelatedProduct.vue";
+import HowItWork from "./components/section/HowItWork.vue";
+
+import Footer from "./pages/Footer.vue";
+import FooterSection from "./components/section/FooterSection.vue";
+
+app.component("HeaderMain", HeaderMain);
+app.component("LogoLink", LogoLink);
+app.component("ButtonLink", ButtonLink);
+app.component("ButtonGroup", ButtonGroup);
+
+app.component("FullScreenSlider", FullScreenSlider);
+app.component("Featured", Featured);
+app.component("SectionHeading", SectionHeading);
+app.component("ProductsContainer", ProductsContainer);
+app.component("RelatedProduct", RelatedProduct);
+app.component("MainSectionHeading", MainSectionHeading);
+
+app.component("HowItWork", HowItWork);
+
+app.component("Footer", Footer);
+app.component("FooterSection", FooterSection);
+
+app.use(router).mount("#app");
--- UNIQUE/frontend/src/router/index.js
+++ UNIQUE/frontend/src/router/index.js
@@ -1,26 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import('../pages/Home.vue'),
+    component: () => import("../pages/Home.vue"),
   },
-]
+  {
+    path: "/product-details-v1-:id",
+    name: "ProductDetail",
+    component: () => import("../pages/ProductDetail.vue"),
+    props: true,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior (to, from, savedPosition) {
+  scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
-      return savedPosition
+      return savedPosition;
     } else {
       return {
         left: 0,
-        top: 0
-      }
+        top: 0,
+      };
     }
-  }
+  },
 });
 
-export default router
+export default router;
