f560820f36a7d174c818a9d34799926a5b7eb56d
김범주 <kbjid17@naver.com>
2022-03-28 13:29:54 +0900

    FEAT: NFT 조회 기능 구현

--- UNIQUE/backend/backend/src/main/java/com/ssafy/unique/api/response/NftRes.java
+++ UNIQUE/backend/backend/src/main/java/com/ssafy/unique/api/response/NftRes.java
@@ -1,15 +1,5 @@
 package com.ssafy.unique.api.response;
 
-<<<<<<< HEAD
-import com.ssafy.unique.api.request.NftReq;
-
-import java.util.List;
-
-public class NftRes {
-
-    private Long nft_req;
-    private Long nft_author_seq;
-=======
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,5 +9,4 @@ public class NftRes {
 	private int result;
 	private Long nftSeq;
 	private String nftMetadataUri;
->>>>>>> a0491097dce360acc914706ec6b79fcbc2de4a93
 }
--- UNIQUE/backend/backend/src/main/java/com/ssafy/unique/db/entity/Nft.java
+++ UNIQUE/backend/backend/src/main/java/com/ssafy/unique/db/entity/Nft.java
@@ -16,7 +16,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-
 @Entity
 @Getter
 @ToString
@@ -26,55 +25,38 @@ import lombok.ToString;
 @DynamicInsert
 @DynamicUpdate
 public class Nft {
-	
+
 	@Id
 	@Column(name = "NFT_SEQ")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long nftSeq;
-	
+
 	@Column(name = "NFT_AUTHOR_SEQ")
 	private Long nftAuthorSeq;
-	
+
 	@Column(name = "NFT_AUTHOR_NAME")
 	private String nftAuthorName;
-	
+
 	@Column(name = "NFT_OWNER_SEQ")
 	private Long nftOwnerSeq;
-	
-<<<<<<< HEAD
-	@Column(name = "NFT_AUTHOR_SEQ") //이거 NFT_AUTHOR_SEQ 아닌가?
-	private Long nftAuthorSeq;
 
-=======
->>>>>>> a0491097dce360acc914706ec6b79fcbc2de4a93
 	@Column(name = "NFT_OWNER_ADDRESS")
 	private String nftOwnerAddress;
-	
-	
+
 	@Column(name = "NFT_WORK_URI")
 	private String nftWorkUri;
 	@Column(name = "NFT_METADATA_URI")
 	private String nftMetadataUri;
 	@Column(name = "NFT_TOKEN_ID")
 	private Long nftTokenId;
-	
-<<<<<<< HEAD
-	@Column(name = "NFT_HASH")
-	private String nftHash;
 
-	@Column(name = "NFT_DESCRIPTION")
-	private String nftDescription;
-
-=======
-	
 	@Column(name = "NFT_NAME")
 	private String nftName;
 
 	@Column(name = "NFT_TYPE")
 	private String nftType;
-	
+
 	@Column(name = "NFT_DESCRIPTION")
 	private String nftDescription;
->>>>>>> a0491097dce360acc914706ec6b79fcbc2de4a93
-	
+
 }
